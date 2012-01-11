aa937d18a21810984a460398e4050a4417423622
Ben <benbenben001@gmail.com>
2012-01-11 23:52:25 +0000

    Array logic fix

--- src/de/bananaco/report/ReportPlugin.java
+++ src/de/bananaco/report/ReportPlugin.java
@@ -33,7 +33,7 @@ public class ReportPlugin extends JavaPlugin {
 	}
 
 	public void log(String message) {
-		System.out.println("[bReport] "+message);
+		System.out.println("[bR] "+message);
 	}
 
 	public void registerPermissions() {
@@ -58,7 +58,7 @@ public class ReportPlugin extends JavaPlugin {
 		// Permission check for players
 		if(sender instanceof Player) {
 			if(!sender.hasPermission("breport."+cname)) {
-				sender.sendMessage(ChatColor.RED+"[bReport] You don't have permission to use the '/"+cname+"' command.");
+				sender.sendMessage(ChatColor.RED+"[bR] You don't have permission to use the '/"+cname+"' command.");
 				return true;
 			}
 		}
@@ -70,18 +70,18 @@ public class ReportPlugin extends JavaPlugin {
 				String[] report = args;
 				// And file the report
 				Report r = rm.createReport(reporter, report, ((Player) sender).getLocation());
-				sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"A report has been filed for you - ID: "+ChatColor.AQUA+r.getID());
+				sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"A report has been filed for you - ID: "+ChatColor.AQUA+r.getID());
 				// Inform all online admins
 				for(Player player : getServer().getOnlinePlayers()) {
 					if(player.hasPermission("breport.read")) {
-						player.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"A new report has been filed by "+ChatColor.AQUA+reporter+ChatColor.GRAY+" - ID: "+ChatColor.AQUA+r.getID());
+						player.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"A new report has been filed by "+ChatColor.AQUA+reporter+ChatColor.GRAY+" - ID: "+ChatColor.AQUA+r.getID());
 					}
 				}
 				// Log to console too
 				log("A new report has been filed by "+reporter+" - ID: "+r.getID());
 				return true;
 			} else {
-				sender.sendMessage(ChatColor.RED+"[bReport] Only players can use the '/report' command!");
+				sender.sendMessage(ChatColor.RED+"[bR] Only players can use the '/report' command!");
 				return true;
 			}
 		}
@@ -91,7 +91,7 @@ public class ReportPlugin extends JavaPlugin {
 				List<String> prints = new ArrayList<String>();
 				// How many shown?
 				int shown = 0;
-				int show = reports.size();
+				int show = reports.size()-1;
 				if(show > 6)
 					show = 6;
 				for(int i=show; i>=0; i--) {
@@ -100,7 +100,7 @@ public class ReportPlugin extends JavaPlugin {
 					prints.add(ChatColor.AQUA+r.getReporter()+ChatColor.GRAY+" - ID: "+ChatColor.AQUA+r.getID());
 					shown++;
 				}
-				sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"Showing "+ChatColor.AQUA+shown+"/"+reports.size()+ChatColor.GRAY+" unread reports");
+				sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Showing "+ChatColor.AQUA+shown+"/"+reports.size()+ChatColor.GRAY+" unread reports");
 				// New line for each report
 				if(reports.size() > 0)
 					for(int i=0; i<prints.size(); i++)
@@ -111,12 +111,13 @@ public class ReportPlugin extends JavaPlugin {
 			} else {
 				String id = args[0];
 				if(rm.getReport(id) == null) {
-					sender.sendMessage(ChatColor.RED+"[bReport] No report with that id, use '/read' to see all unresolved reports.");
+					sender.sendMessage(ChatColor.RED+"[bR] No report with that id, use '/read' to see all unresolved reports.");
 					return true;
 				} else {
 					Report report = rm.getReport(id);
 					// Send the report data
-					sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"Report by: "+ChatColor.AQUA+report.getReporter());
+					sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Reporter: "+ChatColor.AQUA+report.getReporter()+ChatColor.GRAY+" ID: "+ChatColor.AQUA+report.getID());
+					sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Status: "+ChatColor.AQUA+(report.getResolved()?"resolved":"open"));
 					sender.sendMessage(ChatColor.GRAY+"** "+report.getReport());
 					return true;
 				}
@@ -124,20 +125,20 @@ public class ReportPlugin extends JavaPlugin {
 		} else if(cname.equals("resolve") && args.length > 0) {
 			String id = args[0];
 			if(rm.getReport(id) == null) {
-				sender.sendMessage(ChatColor.RED+"[bReport] No report with that id, use '/read' to see all unresolved reports.");
+				sender.sendMessage(ChatColor.RED+"[bR] No report with that id, use '/read' to see all unresolved reports.");
 				return true;
 			} else {
 				Report report = rm.getReport(id);
 				if(report.getResolved()) {
-					sender.sendMessage(ChatColor.RED+"[bReport] That report is already resolved!");
+					sender.sendMessage(ChatColor.RED+"[bR] That report is already resolved!");
 					return true;
 				}
 				report.setResolved(true);
-				sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"Report resolved.");
+				sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Report resolved.");
 				// Inform the player that their report has been resolved if they are online
 				if(getServer().getPlayerExact(report.getReporter()) != null) {
 					Player player = getServer().getPlayerExact(report.getReporter());
-					player.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"A report you filed has been resolved by "+ChatColor.AQUA+getName(sender));
+					player.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"A report you filed has been resolved by "+ChatColor.AQUA+getName(sender));
 					player.sendMessage(ChatColor.GRAY+"** "+report.getReport());
 				}
 				return true;
@@ -145,34 +146,34 @@ public class ReportPlugin extends JavaPlugin {
 		} else if(cname.equals("unresolve") && args.length > 0) {
 			String id = args[0];
 			if(rm.getReport(id) == null) {
-				sender.sendMessage(ChatColor.RED+"[bReport] No report with that id, use '/read' to see all unresolved reports.");
+				sender.sendMessage(ChatColor.RED+"[bR] No report with that id, use '/read' to see all unresolved reports.");
 				return true;
 			} else {
 				Report report = rm.getReport(id);
 				if(!report.getResolved()) {
-					sender.sendMessage(ChatColor.RED+"[bReport] That report is not yet resolved!");
+					sender.sendMessage(ChatColor.RED+"[bR] That report is not yet resolved!");
 					return true;
 				}
 				report.setResolved(false);
-				sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"Report unresolved.");
+				sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Report unresolved.");
 				return true;
 			}
 		} else if(cname.equals("gotoreport") && args.length > 0) {
 			// Again, only players can do this one
 			if(!(sender instanceof Player)) {
-				sender.sendMessage(ChatColor.RED+"[bReport] Only players can use the '/gotoreport' command!");
+				sender.sendMessage(ChatColor.RED+"[bR] Only players can use the '/gotoreport' command!");
 				return true;
 			}
 			String id = args[0];
 			if(rm.getReport(id) == null) {
-				sender.sendMessage(ChatColor.RED+"[bReport] No report with that id, use '/read' to see all unresolved reports.");
+				sender.sendMessage(ChatColor.RED+"[bR] No report with that id, use '/read' to see all unresolved reports.");
 				return true;
 			} else {
 				Player player = (Player) sender;
 				Report r = rm.getReport(id);
 				Location loc = rm.getLocation(r.getLocation());
 				player.teleport(loc);
-				sender.sendMessage(ChatColor.AQUA+"[bReport] "+ChatColor.GRAY+"Teleported to the location of the report - ID: "+ChatColor.AQUA+r.getID());
+				sender.sendMessage(ChatColor.AQUA+"[bR] "+ChatColor.GRAY+"Teleported to the location of the report - ID: "+ChatColor.AQUA+r.getID());
 				return true;
 			}
 		} else if(cname.equals("modchat") && args.length > 0) {
@@ -185,7 +186,7 @@ public class ReportPlugin extends JavaPlugin {
 			// Send the message to online mods/admins
 			for(Player player : getServer().getOnlinePlayers()) {
 				if(player.hasPermission("breport.modchat")) {
-					player.sendMessage(ChatColor.GRAY+"[bReport] "+ChatColor.AQUA+name+ChatColor.GRAY+": "+message);
+					player.sendMessage(ChatColor.GRAY+"[bR] "+ChatColor.AQUA+name+ChatColor.GRAY+": "+message);
 				}
 			}
 			// Also log it to the console
